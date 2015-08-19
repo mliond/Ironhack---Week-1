be29b4850ec4d79c2325b51e57f6e3e865af55e7
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-08-19 22:14:24 +0200

    Messed up the keynote.rb, will revert to older one

--- Day 3/Keynote/keynote.rb
+++ Day 3/Keynote/keynote.rb
@@ -5,50 +5,56 @@ class DBOperations
 
     def read_text_from_file
       @arr = IO.read("slides.txt")
-      split_the_read_text
+      split_the_read_text_into_array
     end
 
-    def split_the_read_text
-      @slide_text = @arr.split("\n----\n").to_a
+    def split_the_read_text_into_array
+      @arr.split("\n----\n").to_a
     end
 end
 
-class ShowTime
+class SlideBuilder
 
-  def initialize(slide_text)
+  def initialize(slide_text, slide_number)
     @slide_text = slide_text
-    @slide_number = 0
-  end
-
-  def change_slide_number(item)
-    @slide_number = @slide_number + item
-    create_a_slide
+    @slide_number = slide_number
   end
 
   def create_empty_lines
     i = 1
     num = 11
       while i < num  do
-      puts""
-      i +=1
+        puts""
+        i +=1
       end
   end
 
   def create_center_line
-    # @empty_spaces = (80 - @slide_text[0].length) / 2
-    text = @slide_text[@slide_number]
-    @centered_line = text.center(80, " ")
+    @slide_text[@slide_number].center(80, " ")
   end
 
   def create_a_slide
+    # I can't get rid of the puts somehow
     puts create_empty_lines
     puts create_center_line
     puts create_empty_lines
+    print @slide_number + 1
+  end
+
+end
+
+class Presenter
+  def initialize(finished_slide)
+    @finished_slide = finished_slide
+    @slide_number = 0
+  end
+
+  def show_the_slide
+    puts @finished_slide
     prompt_for_command
   end
 
   def prompt_for_command
-    puts @slide_number + 1
     @next_or_previous = gets.chomp.upcase
     evaluate_prompt_for_command
   end
@@ -58,16 +64,22 @@ class ShowTime
       change_slide_number(1)
     elsif @next_or_previous == "previous".upcase
       change_slide_number(-1)
-    else
-      puts "error0"
     end
   end
 
-
+  def change_slide_number(change)
+    @slide_number = @slide_number + change
+    return @slide_number
+  end
 end
 
-my_db = DBOperations.new
-slide_text = my_db.read_text_from_file
+text_array = DBOperations.new
+each_slide = SlideBuilder.new(text_array, 0)
+my_presenter = Presenter.new(each_slide)
 
-my_showtime = ShowTime.new(slide_text)
-my_showtime.create_a_slide
+text_array.read_text_from_file
+each_slide.create_a_slide
+
+
+
+my_presenter.show_the_slide
