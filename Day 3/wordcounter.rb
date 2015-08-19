392bf131dbcc031f3ba2c326ab093359b38fc433
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-08-19 17:01:35 +0200

    Finished the last iteration of the wordcounter exercise, including a IO.read for the access data.

--- Day 3/wordcounter.rb
+++ Day 3/wordcounter.rb
@@ -1,22 +1,54 @@
-class Login
+class CorrectAccessData
 
-	def prompt_access
+	attr_reader :correct_username, :correct_password
+
+	def initialize(hardcode_or_file)
+		@hardcode_or_file = hardcode_or_file.upcase
+	end
+
+	def get_correct_access_data
+		if @hardcode_or_file = "file".upcase
+			read_correct_user_and_password_from_file
+		elsif @hardcode_or_file = "hardcode".upcase
+			hardcode_correct_user_and_password
+		else
+			puts "error0"
+		end
+	end
+
+	def read_correct_user_and_password_from_file
+		read_data = IO.read("access_data.txt").to_s
+		@correct_username = read_data.split.first
+		@correct_password = read_data.split.last
+	end
+	
+	def hardcode_correct_user_and_password
+		@correct_username = "test"
+		@correct_password = "abcd"
+	end
+end
+
+class Login
+	def initialize access_data
+		@access_data = access_data
+	end
+
+	def prompt_for_user_and_password
 		puts "username:"
 		@username = gets.chomp.to_s
 		puts "password:"
 		@password = gets.chomp.to_s
 		evaluate_username
 	end
-	
+
 	def evaluate_username
-		if @username.upcase == "test".upcase && @password.upcase == "abcd".upcase
+		if @username.upcase == @access_data.correct_username.upcase && @password.upcase == @access_data.correct_password.upcase
 			puts "correct username & password"
 			WordCounter.new.prompt_text
 		else 
 			puts "wrong username / password"
 		end
 	end
-
 end
 
 
@@ -34,5 +66,9 @@ class WordCounter
 
 end
 
-login = Login.new
-login.prompt_access 
+#Put file or hardcode to switch methods 
+my_correct_access_data = CorrectAccessData.new("file")
+my_correct_access_data.get_correct_access_data
+
+login = Login.new(my_correct_access_data)
+login.prompt_for_user_and_password 
