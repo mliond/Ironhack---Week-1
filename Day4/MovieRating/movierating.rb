259d14d892b6732e494fd96aeddf1c5b73776404
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-08-20 16:46:58 +0200

    some progress on the movie rating app

--- Day4/MovieRating/movierating.rb
+++ Day4/MovieRating/movierating.rb
@@ -1,10 +1,11 @@
 require 'awesome_print'
+require 'rubygems'
 require 'imdb'
 require 'pry'
 
 class TextReader
   def read_from_file
-    movies_txt = IO.read("movies.txt")
+    @movies_txt = IO.read("movies.txt")
     @movies_txt
     turn_into_arr
   end
@@ -20,11 +21,33 @@ class LookOnImdb
     @movies_arr = movies_arr
   end
 
-  def look_on_imdb
-    i = movies_arr[0]
-    puts i.movies.rating
+  def look_for_rating
+    @ratings_arr = @movies_arr.map do |n|
+      i = Imdb::Search.new(n).movies[0].rating
+    end
+    round_ratings
+  end
+
+  def round_ratings
+    @rounded_ratings = @ratings_arr.map {|rating| rating.to_i.round }
+    @rounded_ratings
   end
 end
 
+class LayoutBuilder
+  def initialize(movies_arr, ratings_arr)
+    @movies_arr = movies_arr
+    @ratings_arr = ratings_arr
+  end
+
+  def build_ratings
+	  @ratings_arr.each do |n|
+    	puts "|" + "#|\n" * n
+    end
+  end
+
+end
+
 movies_arr = TextReader.new.read_from_file
-LookOnImdb.new(movies_arr).look_on_imdb
+ratings_arr = LookOnImdb.new(movies_arr).look_for_rating
+LayoutBuilder.new(movies_arr, ratings_arr).build_ratings
